4329e83bb190e9feee670036e45747e40f87f78e
mahidhargudipudi <119840056+mahidhargudipudi@users.noreply.github.com>
2022-12-28 21:12:53 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     printf("%d ",n);  while(n>1)  {      if(n%2==0)      n=n/2;      else      n=n*3+1;      printf("%d ",n);  } }

---
+++ Collatz_Sequence_1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    printf("%d ",n);
+ while(n>1)
+ {
+     if(n%2==0)
+     n=n/2;
+     else
+     n=n*3+1;
+     printf("%d ",n);
+ }
+}
