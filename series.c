e7098aad36d5d1f3512fc28d281ffbc7ae18b93e
mahidhargudipudi <119840056+mahidhargudipudi@users.noreply.github.com>
2022-12-29 21:32:26 +0530

    #include<stdio.h> int main() {     int n,i,a=1,b=1;     scanf("%d",&n);     printf("0 ");     printf("%d %d ",b,a);     for(i=4;i<=n;i++)     {       if(i%2==0)       {           a=a*3;           printf("%d ",a*3);       }       else       {           b=b*2;           printf("%d ",b);       }     } }

--- series.c
+++ series.c
@@ -1,19 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,a=2,b=1;
+    int n,i,a=1,b=1;
     scanf("%d",&n);
+    printf("0 ");
     printf("%d %d ",b,a);
-    for(i=1;i<n;i++)
+    for(i=4;i<=n;i++)
     {
       if(i%2==0)
       {
-          a=a*3+2;
-          printf("%d ",a);
+          a=a*3;
+          printf("%d ",a*3);
       }
       else
       {
-          b=b*2+1;
+          b=b*2;
           printf("%d ",b);
       }
     }
